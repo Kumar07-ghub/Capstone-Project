70c3ee90274ff2eb0ddeb397d65b7822b6a435a6
Kumar07-ghub <tabburi2974@conestogac.on.ca>
2025-06-23 00:38:06 -0400

    Update script.js
    js code created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+// JS code 
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const hasSeenModal = localStorage.getItem('salesModalShown');
+
+    if (!hasSeenModal) {
+      const salesModal = new bootstrap.Modal(document.getElementById('salesModal'));
+      salesModal.show();
+
+      localStorage.setItem('salesModalShown', 'true');
+    }
+  });
 
